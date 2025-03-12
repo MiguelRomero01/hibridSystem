6e05785123511c49d91c9fcbec31b785c71ea261
Pablo <pabloroncancio04mail.com>
2025-03-12 08:30:30 -0500

    cambios papi

--- Views/auth/register/register.js
+++ Views/auth/register/register.js
@@ -1,28 +1,27 @@
-import { POSTMasterPassword } from "../../../BackEnd/Queries/POST/auth/POSTMasterPassword.js";
-import { POSTuserCredentials } from "../../../BackEnd/Queries/POST/auth/POSTuserCredentials.js";
-import { hashPassword } from "../../../BackEnd/services/cryptPassword.js";
+import { POSTMasterPassword } from '../../../BackEnd/Queries/POST/auth/POSTMasterPassword.js'
+import { POSTuserCredentials } from '../../../BackEnd/Queries/POST/auth/POSTuserCredentials.js'
+import { hashPassword } from '../../../BackEnd/services/cryptPassword.js'
 
-document.getElementById("formRegistro").addEventListener("submit", async function (event) {
-    event.preventDefault(); // Evita el envío automático del formulario
+document.getElementById('formRegistro').addEventListener('submit', async function (event) {
+	event.preventDefault() // Evita el envío automático del formulario
 
-    let username = document.getElementById("usuario").value;
-    let password = document.getElementById("clave").value;
-    let password2 = document.getElementById("clave_confirmacion").value;
-    let masterPassword = document.getElementById("clave_maestra").value;
+	let username = document.getElementById('usuario').value
+	let password = document.getElementById('clave').value
+	let password2 = document.getElementById('clave_confirmacion').value
+	let masterPassword = document.getElementById('clave_maestra').value
 
-    if (password === password2) {
-        try {
-            const hashedPassword = await hashPassword(password);
-            await POSTuserCredentials(username, hashedPassword);
+	if (password === password2) {
+		try {
+			const hashedPassword = await hashPassword(password)
+			await POSTuserCredentials(username, hashedPassword)
 
-            const hashedMasterPassword = await hashPassword(masterPassword)
-            await POSTMasterPassword(username, hashedMasterPassword)
-            
-            //redirige SOLO si las credenciales fueron creadas adecuadamente
-            window.location.href = "../login/login.html"; 
+			const hashedMasterPassword = await hashPassword(masterPassword)
+			await POSTMasterPassword(username, hashedMasterPassword)
 
-        } catch (error) {
-            console.error("Error al registrar usuario:", error);
-        }
-    } 
-});
+			//redirige SOLO si las credenciales fueron creadas adecuadamente
+			window.location.href = '/index.html'
+		} catch (error) {
+			console.error('Error al registrar usuario:', error)
+		}
+	}
+})
